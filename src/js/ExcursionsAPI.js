6a6820ba7dc5c3cb437cab7a727e3407511b0f3d
Malgorzata Kowacka <kowackag@gmail.com>
2021-07-30 12:31:56 +0200

    Create addExcursion, updateExcursion, removeExcursion in admin.js

--- src/js/ExcursionsAPI.js
+++ src/js/ExcursionsAPI.js
@@ -1,56 +1,45 @@
 class ExcursionsAPI {
     constructor() {
         this.url = 'http://localhost:3000/excursions';
-        this.prototype = document.querySelector('.excursions__item--prototype');
-    }
-    loadData() {
-        this._fetch()
-            .then(data => {
-                this.insertData(data);
-            })
-            .catch(err => console.error(err));
-    }
-    insertData(data) {
-        const ulEl = document.querySelector('.excursions');
-        ulEl.innerHTML = '';
-
-        data.forEach(element => {
-            const newLiEl = this._createLiEl(element);
-            ulEl.appendChild(newLiEl);
-        })
+        // this.prototype = document.querySelector('.excursions__item--prototype');
     }
 
-    addData() {
-        const form = document.querySelector('.form');
-        form.addEventListener('submit', e => {
-            e.preventDefault();
-            const {
-                name,
-                description,
-                adultsPrice,
-                childrenPrice
-            } = e.target.elements;
-            const data = {
-                name: name.value,
-                description: description.value,
-                adultsPrice: adultsPrice.value,
-                childrenPrice: childrenPrice.value
-            }
-            const options = {
-                method: 'POST',
-                body: JSON.stringify(data),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            }
-            console.log(data)
-            return this._fetch(options)
-                .catch(err => console.error(err))
-                .finally(() => this.loadData());
-        })
+loadData() {
+      return this._fetch()
     }
-    _fetch(options) {
-        const url = this.url;
+
+addData(data) {
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        console.log(data);
+        return this._fetch(options);
+    }
+
+removeData(id) {
+        const options = {
+            method: 'DELETE'
+        };
+        return this._fetch(options, `/${id}`);
+    }
+
+updateData(id, data) {
+        const options = {
+            method: 'PUT',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        return this._fetch(options, `/${id}`);
+    }
+
+_fetch(options, additionalPath = '') {
+        const url = this.url + additionalPath;
         return fetch(url, options)
             .then(resp => {
                 if (resp.ok) {
@@ -59,19 +48,76 @@ class ExcursionsAPI {
                 return Promise.reject(resp);
             });
     }
-    _createLiEl(element) {
-        const newLiEl = this.prototype.cloneNode(true);
-        newLiEl.dataset.id = element.id;
-        const titleEl = newLiEl.querySelector('.excursions__title');
-        titleEl.innerText = element.name;
-        const descriptionEl = newLiEl.querySelector('.excursions__description');
-        descriptionEl.innerText = element.description;
-        newLiEl.classList.remove('excursions__item--prototype');
-        const fieldList = newLiEl.querySelectorAll('.excursions__field-name');
-        fieldList[0].innerHTML = `Dorosły:<strong>${element.adultsPrice}</strong>PLN`;
-        fieldList[1].innerHTML = `Dziecko:<strong>${element.childrenPrice}</strong>PLN`
-        return newLiEl;
-    }
+
+    // ----------------------------------------------------------------
+    // loadData() {
+    //     this._fetch()
+    //         .then(data => {
+    //             this.insertData(data);
+    //         })
+    //         .catch(err => console.error(err));
+    // }
+    // insertData(data) {
+    //     const ulEl = document.querySelector('.excursions');
+    //     ulEl.innerHTML = '';
+
+    //     data.forEach(element => {
+    //         const newLiEl = this._createLiEl(element);
+    //         ulEl.appendChild(newLiEl);
+    //     })
+    // }
+
+    // addData() {
+    //     const form = document.querySelector('.form');
+    //     form.addEventListener('submit', e => {
+    //         e.preventDefault();
+    //         const {
+    //             name,
+    //             description,
+    //             adultsPrice,
+    //             childrenPrice
+    //         } = e.target.elements;
+    //         const data = {
+    //             name: name.value,
+    //             description: description.value,
+    //             adultsPrice: adultsPrice.value,
+    //             childrenPrice: childrenPrice.value
+    //         }
+    //         const options = {
+    //             method: 'POST',
+    //             body: JSON.stringify(data),
+    //             headers: {
+    //                 'Content-Type': 'application/json'
+    //             }
+    //         }
+    //          return this._fetch(options)
+    //             .catch(err => console.error(err))
+    //             .finally(() => this.loadData());
+    //     })
+    // }
+    // _fetch(options) {
+    //     const url = this.url;
+    //     return fetch(url, options)
+    //         .then(resp => {
+    //             if (resp.ok) {
+    //                 return resp.json();
+    //             }
+    //             return Promise.reject(resp);
+    //         });
+    // }
+    // _createLiEl(element) {
+    //     const newLiEl = this.prototype.cloneNode(true);
+    //     newLiEl.dataset.id = element.id;
+    //     const titleEl = newLiEl.querySelector('.excursions__title');
+    //     titleEl.innerText = element.name;
+    //     const descriptionEl = newLiEl.querySelector('.excursions__description');
+    //     descriptionEl.innerText = element.description;
+    //     newLiEl.classList.remove('excursions__item--prototype');
+    //     const fieldList = newLiEl.querySelectorAll('.excursions__field-name');
+    //     fieldList[0].innerHTML = `Dorosły:<strong>${element.adultsPrice}</strong>PLN`;
+    //     fieldList[1].innerHTML = `Dziecko:<strong>${element.childrenPrice}</strong>PLN`
+    //     return newLiEl;
+    // }
     // _clearElement(element) {
     //     element.innerHTML = '';
     // }
--- src/js/admin.js
+++ src/js/admin.js
@@ -2,18 +2,104 @@ import './../css/admin.css';
 
 import ExcursionsAPI from './ExcursionsAPI';
 
-
 document.addEventListener('DOMContentLoaded', init);
+const excursions = new ExcursionsAPI();
+const prototype = document.querySelector('.excursions__item--prototype');
+const ulEl = document.querySelector('.excursions');
 
 function init() {
     console.log('admin');
-    // const api = new API();
-    const excursions = new ExcursionsAPI();
-    excursions.loadData();
+    loadExcursions();
+    addExcursions();
+    removeExcursions();
+    updateExcursions();
+}
 
-    excursions.addData();
-    // excursions.insertData();
-    // excursions.remove();
-    // excursions.add();
-    // excursions.update();
+function loadExcursions() {
+    excursions.loadData()
+        .then(data => insertExcursions(data))
+        .catch(err => console.error(err))
+}
+
+function insertExcursions(data) {
+    ulEl.innerHTML = '';
+    data.forEach(element => {
+        const newLiEl = createLiEl(element);
+        ulEl.appendChild(newLiEl);
+    })
+}
+
+function addExcursions() {
+    const form = document.querySelector('.form');
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        const {
+            name,
+            description,
+            adultsPrice,
+            childrenPrice
+        } = e.target.elements;
+        const data = {
+            name: name.value,
+            description: description.value,
+            adultsPrice: adultsPrice.value,
+            childrenPrice: childrenPrice.value
+        }
+        excursions.addData(data)
+            .catch(err => console.error(err))
+            .finally(() => loadExcursions());
+    })
+}
+
+function removeExcursions() {
+    ulEl.addEventListener('click', e => {
+        e.preventDefault();
+        if (e.target.className.includes('excursions__field-input--remove')) {
+            const id = e.target.parentElement.parentElement.parentElement.dataset.id;
+            excursions.removeData(id)
+                .catch(err => console.error(err))
+                .finally(() => loadExcursions());
+        }
+    })
+}
+
+function updateExcursions() {
+    ulEl.addEventListener('click', e => {
+        e.preventDefault();
+        if (e.target.className.includes('excursions__field-input--update')) {
+            const liEl = e.target.parentElement.parentElement.parentElement;
+            const childrenList = [...liEl.querySelectorAll('.excursions__field-name strong, .excursions__description, .excursions__title')];
+            if (e.target.value === 'edytuj') {
+                e.target.value = 'zapisz';
+                childrenList.forEach(item => item.contentEditable = 'true');
+            } else {
+                const id = liEl.dataset.id;
+                const data = {
+                    name: childrenList[0].innerText,
+                    description: childrenList[1].innerText,
+                    adultsPrice: childrenList[2].innerText,
+                    childrenPrice: childrenList[3].innerText
+                }
+                e.target.value = 'edytuj';
+                childrenList.forEach(item => item.contentEditable = 'false')
+                excursions.updateData(id, data)
+                    .catch(err => console.error(err))
+                    .finally(() => loadExcursions());
+            }
+        }
+    })
+}
+
+function createLiEl(element) {
+    const newLiEl = prototype.cloneNode(true);
+    newLiEl.dataset.id = element.id;
+    const titleEl = newLiEl.querySelector('.excursions__title');
+    titleEl.innerText = element.name;
+    const descriptionEl = newLiEl.querySelector('.excursions__description');
+    descriptionEl.innerText = element.description;
+    newLiEl.classList.remove('excursions__item--prototype');
+    const fieldList = newLiEl.querySelectorAll('.excursions__field-name');
+    fieldList[0].innerHTML = `Dorosły:<strong>${element.adultsPrice}</strong>PLN`;
+    fieldList[1].innerHTML = `Dziecko:<strong>${element.childrenPrice}</strong>PLN`
+    return newLiEl;
 }
